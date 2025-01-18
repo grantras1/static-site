eccd64b2bf8d4d7b8f5fb11354c9f3c7019fa228
grantras1 <grasmussen66@gmail.com>
2025-01-18 09:54:31 -0500

    Added node splitting for images and links

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -1,4 +1,9 @@
-from textnode import TextNode
+import re
+from time import sleep
+
+from textnode import TextNode, TextType
+from markdown_extraction import extract_markdown_images, extract_markdown_links
+
 
 def generate_split_text(text, delimiter):
     ret = []
@@ -10,6 +15,7 @@ def generate_split_text(text, delimiter):
         ret.append(delimiter)
     if prepend_delimiter:
         ret.insert(0, delimiter)
+    print(ret)
     return ret
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -32,3 +38,41 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             else:
                 prev_text = text
     return new_nodes
+
+def get_indices_of_match(pattern, text):
+    idx = 0
+    idx_total = 0
+    indices = []
+    while idx_total < len(text) - 1:
+        match = re.search(pattern, text[idx:])
+        if not match:
+            break
+        indices.append((idx + match.start(), idx + match.end()))
+        idx = match.end()
+        idx_total += idx
+    return indices
+
+def split_nodes_regex(old_nodes, pattern, type):
+    new_nodes = []
+    for node in old_nodes:
+        indices = get_indices_of_match(pattern, node.text)
+        if len(indices) == 0:
+            new_nodes.append(node)
+            continue
+        idx = 0
+        for regex_index in indices:
+            if regex_index[0] > 0:
+                new_nodes.append(TextNode(node.text[idx:regex_index[0]], node.text_type))
+            link = extract_markdown_links(node.text[idx:])[0]
+            new_nodes.append(TextNode(link[0], type, link[1]))
+            idx += regex_index[1]
+        if indices[-1][1] < len(node.text):
+            new_nodes.append(TextNode(node.text[indices[-1][1]:], node.text_type))
+    return new_nodes
+
+def split_nodes_image(old_nodes):
+    return split_nodes_regex(old_nodes, r"\!\[.*?\]\(.*?\)", TextType.IMAGES)
+
+def split_nodes_link(old_nodes):
+    return split_nodes_regex(old_nodes, r"\[.*?\]\(.*?\)", TextType.LINKS)
+
--- src/test_split_delimiter.py
+++ src/test_split_delimiter.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from split_delimiter import split_nodes_delimiter
+from split_delimiter import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 class TestSplitDelimiter(unittest.TestCase):
     def test_split_delimiter(self):
@@ -34,5 +34,54 @@ class TestSplitDelimiter(unittest.TestCase):
         ]
         self.assertEqual(split_nodes_delimiter([bold_node], "**", TextType.BOLD), expected_value)
 
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.NORMAL
+        )
+        node2 = TextNode(
+            "[google link](google.com) and normal text",
+            TextType.NORMAL
+        )
+        node3 = TextNode(
+            "there are no links here",
+            TextType.NORMAL
+        )
+        expected_nodes = [
+            TextNode("This is text with a link ", TextType.NORMAL),
+            TextNode("to boot dev", TextType.LINKS, "https://www.boot.dev"),
+            TextNode(" and ", TextType.NORMAL),
+            TextNode("to youtube", TextType.LINKS, "https://www.youtube.com/@bootdotdev"),
+            TextNode("google link", TextType.LINKS, "google.com"),
+            TextNode(" and normal text", TextType.NORMAL),
+            TextNode("there are no links here", TextType.NORMAL)
+        ]
+        self.assertEqual(split_nodes_link([node, node2, node3]), expected_nodes)
+
+    def test_split_images(self):
+        self.maxDiff = None
+        node = TextNode(
+            "This is the boot.dev logo ![boot.dev logo](https://www.boot.dev/img/bootdev-logo-full-small.webp) python logo ![python logo](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)",
+            TextType.NORMAL
+        )
+        node2 = TextNode(
+            "![c logo](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/leiue6x.png) and normal text",
+            TextType.NORMAL
+        )
+        node3 = TextNode(
+            "there are no images here",
+            TextType.NORMAL
+        )
+        expected_nodes = [
+            TextNode("This is the boot.dev logo ", TextType.NORMAL),
+            TextNode("boot.dev logo", TextType.IMAGES, "https://www.boot.dev/img/bootdev-logo-full-small.webp"),
+            TextNode(" python logo ", TextType.NORMAL),
+            TextNode("python logo", TextType.IMAGES, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            TextNode("c logo", TextType.IMAGES, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/leiue6x.png"),
+            TextNode(" and normal text", TextType.NORMAL),
+            TextNode("there are no images here", TextType.NORMAL)
+        ]
+
+        self.assertEqual(split_nodes_image([node, node2, node3]), expected_nodes)
 if __name__ == "__main__":
     unittest.main()
